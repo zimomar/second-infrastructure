a00fa8ed87ac4f4d5b9832940ae27bd9a05b6990
Omar Abouslime <omar@fregat.io>
2023-08-22 13:51:50 +0200

    functionnal dolibarr application & its volume

--- infra/instances.tf
+++ infra/instances.tf
@@ -54,32 +54,50 @@ resource "digitalocean_droplet" "s4" {
   vpc_uuid              = digitalocean_vpc.vpc01.id
 }
 
-resource "digitalocean_firewall" "basic_fw" {
-  name                  = "basic-fw"
-  droplet_ids           = [digitalocean_droplet.s0.id, digitalocean_droplet.s1.id, digitalocean_droplet.s2.id,
-    digitalocean_droplet.s3.id, digitalocean_droplet.s4.id]
-
-  inbound_rule {
-    protocol            = "tcp"
-    port_range          = "80"
-    source_addresses    = ["0.0.0.0/0", "::/0"]
-  }
-
+#resource "digitalocean_firewall" "basic_fw" {
+#  name                  = "basic-fw"
+#  droplet_ids           = [digitalocean_droplet.s0.id, digitalocean_droplet.s1.id, digitalocean_droplet.s2.id,
+#    digitalocean_droplet.s3.id, digitalocean_droplet.s4.id]
+#
 #  inbound_rule {
 #    protocol            = "tcp"
-#    port_range          = "1-65535"
-#    source_addresses    = ["192.168.50.0/24"]
+#    port_range          = "80"
+#    source_addresses    = ["0.0.0.0/0", "::/0"]
 #  }
+#
+#  inbound_rule {
+#    protocol            = "tcp"
+#    port_range          = "443"
+#    source_addresses    = ["0.0.0.0/0", "::/0"]
+#  }
+#
+#  inbound_rule {
+#    protocol            = "tcp"
+#    port_range          = "53"
+#    source_addresses    = ["0.0.0.0/0", "::/0"]
+#  }
+#
+#  inbound_rule {
+#    protocol            = "icmp"
+#    source_addresses    = ["0.0.0.0/0", "::/0"]
+#  }
+#
+#  inbound_rule {
+#    protocol            = "tcp"
+#    port_range          = "22"
+#    source_addresses    = ["109.18.226.32/32", "89.87.89.179/32", "83.202.17.247"] # Omar, Mamadou, Allan
+#  }
+#
+#  inbound_rule {
+#    protocol            = "tcp"
+#    port_range          = "3306"
+#    source_addresses    = ["109.18.226.32/32", "89.87.89.179/32", "83.202.17.247"] # Omar, Mamadou, Allan
+#  }
+#
+#  outbound_rule {
+#    protocol            = "tcp"
+#    port_range          = "1-65535"
+#    destination_addresses = ["0.0.0.0/0", "::/0"]
+#  }
+#}
 
-  inbound_rule {
-    protocol            = "tcp"
-    port_range          = "22"
-    source_addresses    = ["109.18.226.32/32", "89.87.89.179/32", "83.202.17.247"] # Omar, Mamadou, Allan
-  }
-
-  inbound_rule {
-    protocol            = "tcp"
-    port_range          = "3306"
-    source_addresses    = ["109.18.226.32/32", "89.87.89.179/32", "83.202.17.247"] # Omar, Mamadou, Allan
-  }
-}
